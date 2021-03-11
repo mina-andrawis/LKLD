2fc4c3aec2bae88b4cae31368c8fdd86dac4e09f
Mina Andrawis <mi.andrawis@gmail.com>
2021-03-11 15:22:20 -0500

    Dialogue Trigger added

--- LionKingdom_Unity/Assets/Scripts/DialogueSystem/Dialogue.cs
+++ LionKingdom_Unity/Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+// passed into the dialogue manager whenever we want to start a new dialogue
+// hosts all info we need for a single dialogue
+// must be marked as serializable in order to show up in the inspector to edit
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[System.Serializable]
+public class Dialogue
+{
+    public string name;
+    public string[] sentences;
+    
 }
