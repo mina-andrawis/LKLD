294ce117b5e92ffcc69a8223abafb575a69c5ab0
Mina Andrawis <mi.andrawis@gmail.com>
2021-03-23 21:05:54 -0400

    readded files from health-bar branch

---
+++ LionKingdom_Unity/Assets/Scripts/HealthBar/EnemyHealth.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   //in order to create variable to store slider
+
+
+public class EnemyHealth : MonoBehaviour
+{
+
+  public int maxHealth = 100;
+  public int currentHealth;
+  public Arrow arrow;
+
+  public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+          //TakeDamage(30);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+      Debug.Log("Damage taken");
+      currentHealth -= damage;
+      healthBar.SetHealth(currentHealth);
+    }
+
+}
