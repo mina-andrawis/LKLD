4ac2671ab6ef2e9f94bc1eabdb299d8a8f7b51d3
Mina Andrawis <maa16s@my.fsu.edu>
2021-04-06 17:38:14 -0400

    Chest opens when player enters radius

--- LionKingdom_Unity/Assets/Scripts/ChestOpen.cs
+++ LionKingdom_Unity/Assets/Scripts/ChestOpen.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.U2D.Animation;
 using UnityEngine;
 
 public class ChestOpen : MonoBehaviour
@@ -12,10 +13,15 @@ public class ChestOpen : MonoBehaviour
     public GameObject Player;
     public GameObject Target;
 
+    public SpriteResolver mySpriteResolver;
+
+    //retrieve spriteLibrary that the SpriteResolver is resolving from
+    public SpriteLibrary spriteLibrary { get; }
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        mySpriteResolver = GetComponent<SpriteResolver>();
         Player = GameObject.FindWithTag("Player");
         Target = GameObject.FindWithTag("Chest");
     }
@@ -23,12 +29,14 @@ public class ChestOpen : MonoBehaviour
     private void Update()
     {
         radius = Vector3.Distance(Player.transform.position, Target.transform.position);
-        Debug.Log(radius);
+        //Debug.Log(radius);
 
         if (radius < 60f )
         {
-            GetComponent<Animator>().SetBool("isOpened",true);
-            Debug.Log("Animation played");
+            //GetComponent<Animator>().SetBool("isOpened",true);
+            //Debug.Log(mySpriteResolver.GetLabel());
+            //Debug.Log("Animation played");
+            mySpriteResolver.SetCategoryAndLabel("Chest", "OpenChest");
         }
     }
 }
