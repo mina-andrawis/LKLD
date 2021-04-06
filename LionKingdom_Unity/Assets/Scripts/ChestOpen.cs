16afa07b23f9e2d91f6fb3c50972b4f4746ba25f
Mina Andrawis <maa16s@my.fsu.edu>
2021-04-06 15:20:57 -0400

    console message sent when player enters redius of chest

--- LionKingdom_Unity/Assets/Scripts/ChestOpen.cs
+++ LionKingdom_Unity/Assets/Scripts/ChestOpen.cs
@@ -4,32 +4,31 @@ using UnityEngine;
 
 public class ChestOpen : MonoBehaviour
 {
-  
 
+    private float radius;
+    private float distance;
 
-    public Animator animator;
+    public Animator anim;
     public GameObject Player;
     public GameObject Target;
 
+
     private void Start()
     {
-        animator = GetComponent<Animator>();
-        
+        anim = GetComponent<Animator>();
+        Player = GameObject.FindWithTag("Player");
+        Target = GameObject.FindWithTag("Chest");
     }
 
     private void Update()
     {
         radius = Vector3.Distance(Player.transform.position, Target.transform.position);
+        Debug.Log(radius);
 
-        if (radius < 5f)
+        if (radius < 60f )
         {
-            animator.SetBool("IsOpen", true);
+            GetComponent<Animator>().SetBool("isOpened",true);
             Debug.Log("Animation played");
-        }
-        else
-        {
-            animator.SetBool("IsOpen", false);
-
         }
     }
 }
