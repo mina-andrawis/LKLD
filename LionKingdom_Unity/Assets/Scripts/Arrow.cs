7c70efdca899c2bbad793b627817bb9eb22ecb36
John Ronzo <jronzo99@gmail.com>
2021-04-11 16:31:11 -0400

    Enemy attack

--- LionKingdom_Unity/Assets/Scripts/Arrow.cs
+++ LionKingdom_Unity/Assets/Scripts/Arrow.cs
@@ -4,52 +4,28 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public int damage = 30;
-    public float speed = 20;
-    public Rigidbody2D rb;
+    public float dieTime;
+    public float damage;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        // fires at set speed
-        rb.velocity = transform.right * speed;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //
-        if (collision.GetComponent<Enemy>())
-        {
-            Debug.Log(collision.name);
-            Enemy enemy = collision.GetComponent<Enemy>();
-
-            // checks if the arrow hit an enemy and deals damage
-            if (enemy != null)
-            {
-                Debug.Log("Damage given: " + damage);
-
-                enemy.TakeDamage(damage);
-            }
-
-            Destroy(gameObject);
-
-        }
-
-        // arrow keeps going
-
-        // add a check for if arrow hits something that isn't the background
-        // like a wall or platform, then just destroy the arrow
-
-        // add check for if arrow goes off the current visible camera screen,
-        // then destroy the arrow
-        else if(collision.CompareTag("Blocks"))
-        {
-            Destroy(gameObject);
-        }
+        StartCoroutine(CountDownTime());
     }
     
-    void OnBecameInvisible() {
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
+    
+    void OnBecameInvisible()
+    {
          Destroy(gameObject);
-     }
+    }
+     
+    IEnumerator CountDownTime()
+    {
+        yield return new WaitForSeconds(dieTime);
+        
+        Destroy(gameObject);
+    }
 }
---
+++ LionKingdom_Unity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 30;
+    public float speed = 20;
+    public Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // fires at set speed
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //
+        if (collision.GetComponent<Enemy>())
+        {
+            Debug.Log(collision.name);
+            Enemy enemy = collision.GetComponent<Enemy>();
+
+            // checks if the bullet hit an enemy and deals damage
+            if (enemy != null)
+            {
+                Debug.Log("Damage given: " + damage);
+
+                enemy.TakeDamage(damage);
+            }
+
+            Destroy(gameObject);
+
+        }
+        else if(collision.CompareTag("Blocks"))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void OnBecameInvisible() {
+         Destroy(gameObject);
+     }
+}
