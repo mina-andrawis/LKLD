a51aee77b0809910959a8f8f560fa610263a7eb6
jronzo99 <jronzo99@gmail.com>
2021-04-16 12:21:21 -0400

    Revert "Fixing merge issues"

--- LionKingdom_Unity/Assets/Scripts/HealthBar/PlayerHealth.cs
+++ LionKingdom_Unity/Assets/Scripts/HealthBar/PlayerHealth.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;   //in order to create variable to store slider
 public class PlayerHealth : MonoBehaviour
 {
 
-  public int maxHealth = 30;
+  public int maxHealth = 100;
   public int currentHealth;
 
   public HealthBar healthBar;
@@ -23,13 +23,24 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      Debug.Log("INISDE");
         if (Input.GetKeyDown(KeyCode.Tab))
         {
           TakeDamage(5);
         }
     }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Potion"))
+        {
+            currentHealth += 10;
+            healthBar.SetHealth(currentHealth);
+            Destroy(other.gameObject);
+        }
+    }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
       currentHealth -= damage;
       healthBar.SetHealth(currentHealth);
