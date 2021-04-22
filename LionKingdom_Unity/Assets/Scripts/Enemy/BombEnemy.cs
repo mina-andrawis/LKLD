c050a60ecb9dcb5e0a7df573d5873b59c7e9f6d9
John Ronzo <jronzo99@gmail.com>
2021-04-22 13:40:24 -0400

    Merge branch 'enemy_bomb' into main

---
+++ LionKingdom_Unity/Assets/Scripts/Enemy/BombEnemy.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BombEnemy : MonoBehaviour
+{
+    public float Speed = 4;
+    public Vector3 LaunchOffset;
+    public bool Thrown;
+    public int damage = 30;
+    public float splashRange = 1;
+    public Animator animator;
+    
+    public Rigidbody2D rb;
+    PlayerMovement target;
+    PlayerHealth health;
+    
+    private void Start()
+    {
+        target = GameObject.FindObjectOfType<PlayerMovement>();
+        health = GameObject.FindObjectOfType<PlayerHealth>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if(Thrown)
+        {
+            var direction = (target.transform.position - transform.position).normalized * Speed;
+            rb.velocity = new Vector2(direction.x, direction.y);
+        }
+        transform.Translate(LaunchOffset);
+        Destroy(gameObject, 5);
+    }
+    
+    public void Update()
+    {
+        if(!Thrown)
+        {
+            transform.position += transform.right * Speed * Time.deltaTime;
+        }
+        
+    }
+    
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(splashRange > 0)
+        {
+            var hitColliders = Physics2D.OverlapCircleAll(transform.position, splashRange);
+            foreach(var hitCollider in hitColliders)
+            {
+                var player = hitCollider.GetComponent<PlayerMovement>();
+                var playerHealth = hitCollider.GetComponent<PlayerHealth>();
+            
+                if(player)
+                {
+                    var closestPoint = hitCollider.ClosestPoint(transform.position);
+                    var distance = Vector3.Distance(closestPoint, transform.position);
+                    var damagePercent = Mathf.InverseLerp(splashRange, 0, distance);
+                    
+                    playerHealth.TakeDamage((int)Math.Round(damagePercent * damage));
+                }
+            }
+        }
+        else
+        {
+            var player = collision.collider.GetComponent<PlayerMovement>();
+            var playerHealth = collision.collider.GetComponent<PlayerHealth>();
+            if(player)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+        animator.SetTrigger("IsExploding");
+        
+        Destroy(gameObject, 1);
+    }
+    
+    void OnBecameInvisible() {
+         Destroy(gameObject);
+     }
+}
